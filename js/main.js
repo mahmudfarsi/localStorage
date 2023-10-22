e0b48c081ea67f97da4241b97521ffcdd894d95b
mahmud farsi <mhfarsi002022@gmail.com>
2023-10-23 02:40:15 +0330

    add script

--- js/main.js
+++ js/main.js
@@ -5,7 +5,45 @@ const filter = $.querySelector('#filter');
 const btnClear = $.querySelector('#btnClear');
 const list = $.querySelector('.list_items');
 let dataObj = JSON.parse(localStorage.getItem('mode'))??[];
+btnSubmit.addEventListener('click',()=>{
+    let inputValue = input.value;
+    dataObj.push(inputValue);
+    localStorage.setItem('mode',JSON.stringify(dataObj))
+    let li = $.createElement('li');
+    li.className = 'items';
+    li.textContent = inputValue
+    let i = $.createElement('i');
+    i.className = 'fa-solid fa-xmark';
+    list.append(li);
+    li.append(i);
+    input.value = '';
+
+});
+dataObj.forEach(element => {
+    let li = $.createElement('li');
+    li.className = 'items';
+    li.textContent = element
+    let i = $.createElement('i');
+    i.className = 'fa-solid fa-xmark';
+    list.append(li);
+    li.append(i);
+});
+list.addEventListener('click',(e)=>{
+    let textContent = e.target.textContent;
+    let findData = dataObj.indexOf(textContent);
+    dataObj.splice(findData,1);
+    if(e.target.className === 'fa-solid fa-xmark'){
+        let parent = e.target.parentElement;
+        parent.style.display = 'none';
+        localStorage.setItem('mode',JSON.stringify(dataObj));
+    }
+});
+btnClear.addEventListener('click',()=>{
+    list.innerHTML = '';
+    input.value = '';
+    filter.value = '';
+    localStorage.clear();
+});
 
 
-
-
+//filter ..............{}
